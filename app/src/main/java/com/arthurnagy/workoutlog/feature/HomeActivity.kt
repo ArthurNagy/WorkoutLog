1b79b114e799fa6210c4cff0d360d874a0c00199
Arthur <arthur.93.nagy@gmail.com>
2018-05-20 11:28:01 +0300

    Remove backdrop component module

--- app/src/main/java/com/arthurnagy/workoutlog/feature/HomeActivity.kt
+++ app/src/main/java/com/arthurnagy/workoutlog/feature/HomeActivity.kt
@@ -6,13 +6,14 @@ import androidx.databinding.DataBindingUtil
 import com.arthurnagy.workoutlog.MainBinding
 import com.arthurnagy.workoutlog.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<MainBinding>(this, R.layout.activity_main)?.also {
+        DataBindingUtil.setContentView<MainBinding>(this, R.layout.activity_home)?.also {
             setSupportActionBar(it.toolbar)
             supportActionBar?.setDisplayShowTitleEnabled(false)
+
         }
     }
 
