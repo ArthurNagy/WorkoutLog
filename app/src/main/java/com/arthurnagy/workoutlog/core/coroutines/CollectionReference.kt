1b57ab205329fc0595f2cad75af5cb9b18710cf1
Arthur <arthur.93.nagy@gmail.com>
2018-06-16 22:52:21 +0300

    Implement coroutine extensions for Firestore classes

--- app/src/main/java/com/arthurnagy/workoutlog/core/coroutines/CollectionReference.kt
+++ app/src/main/java/com/arthurnagy/workoutlog/core/coroutines/CollectionReference.kt
@@ -7,23 +7,42 @@
 package com.arthurnagy.workoutlog.core.coroutines
 
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.Source
 import kotlin.coroutines.experimental.suspendCoroutine
 
-private suspend fun <T : Any> readCollectionReference(collection: CollectionReference, type: Class<T>): List<T> = suspendCoroutine { continuation ->
-    collection.get().addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-            try {
-                val data: List<T> = task.result.toObjects(type)
-                continuation.resume(data)
-            } catch (exception: Exception) {
-                continuation.resumeWithException(exception)
+//region GET
+private suspend fun <T : Any> getCollectionReference(collection: CollectionReference, type: Class<T>, source: Source = Source.DEFAULT): List<T> =
+    suspendCoroutine { continuation ->
+        collection.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                try {
+                    val data: List<T> = task.result.toObjects(type)
+                    continuation.resume(data)
+                } catch (exception: Exception) {
+                    continuation.resumeWithException(exception)
+                }
+            } else {
+                continuation.resumeWithException(task.exception ?: Exception("Failed to read collection from $collection of type: $type"))
             }
+        }
+    }
+
+suspend fun <T : Any> CollectionReference.getList(type: Class<T>, source: Source = Source.DEFAULT): List<T> = getCollectionReference(this, type, source)
+
+suspend inline fun <reified T : Any> CollectionReference.getList(source: Source = Source.DEFAULT): List<T> = getList(T::class.java, source)
+//endregion
+
+//region ADD
+private suspend fun <T : Any> addDocumentToCollection(collection: CollectionReference, data: T): DocumentReference = suspendCoroutine { continuation ->
+    collection.add(data).addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            continuation.resume(task.result)
         } else {
-            continuation.resumeWithException(task.exception ?: Exception("Failed to read collection from $collection of type: $type"))
+            continuation.resumeWithException(task.exception ?: Exception("Failed to add $data to collection: $collection"))
         }
     }
 }
 
-suspend fun <T : Any> CollectionReference.readList(type: Class<T>): List<T> = readCollectionReference(this, type)
-
-suspend inline fun <reified T : Any> CollectionReference.readList(): List<T> = readList(T::class.java)
+suspend fun <T : Any> CollectionReference.add(data: T): DocumentReference = addDocumentToCollection(this, data)
+//endregion
--- app/src/main/java/com/arthurnagy/workoutlog/core/coroutines/DocumentReference.kt
+++ app/src/main/java/com/arthurnagy/workoutlog/core/coroutines/DocumentReference.kt
@@ -7,34 +7,113 @@
 package com.arthurnagy.workoutlog.core.coroutines
 
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.Source
 import kotlin.coroutines.experimental.suspendCoroutine
 
-private suspend fun <T : Any> getDocumentValue(document: DocumentReference, type: Class<T>, source: Source = Source.DEFAULT): T =
+//region GET
+/**
+ * @param document
+ * @param type
+ * @param source
+ * @return
+ */
+private suspend fun <T : Any> getValue(document: DocumentReference, type: Class<T>, source: Source = Source.DEFAULT): T =
     suspendCoroutine { continuation ->
         document.get(source).addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 task.result.toObject(type)?.let { data ->
                     continuation.resume(data)
-                } ?: continuation.resumeWithException(Exception("Failed to read document from $document of type: $type"))
+                } ?: continuation.resumeWithException(Exception("Failed to get document from $document of type: $type"))
             } else {
-                continuation.resumeWithException(task.exception ?: Exception("Failed to read document from $document of type: $type"))
+                continuation.resumeWithException(task.exception ?: Exception("Failed to get document from $document of type: $type"))
             }
         }
     }
 
-suspend fun <T : Any> DocumentReference.getValue(type: Class<T>, source: Source = Source.DEFAULT): T = getDocumentValue(this, type, source)
+/**
+ * @param type
+ * @param source
+ * @return
+ * @receiver
+ */
+suspend fun <T : Any> DocumentReference.get(type: Class<T>, source: Source = Source.DEFAULT): T = getValue(this, type, source)
 
-suspend inline fun <reified T : Any> DocumentReference.getValue(source: Source = Source.DEFAULT): T = getValue(T::class.java, source)
+/**
+ * @param source
+ * @return
+ * @receiver
+ */
+suspend inline fun <reified T : Any> DocumentReference.getValue(source: Source = Source.DEFAULT): T = this.get(T::class.java, source)
+//endregion
 
-private suspend fun <T : Any> setDocumentValue(document: DocumentReference, data: T): Unit = suspendCoroutine { continuation ->
-    document.set(data).addOnCompleteListener { task ->
+//region SET
+
+/**
+ * @param
+ */
+private suspend fun <T : Any> setValue(document: DocumentReference, data: T, setOptions: SetOptions? = null): Unit = suspendCoroutine { continuation ->
+    if (setOptions == null) document.set(data) else document.set(data, setOptions).addOnCompleteListener { task ->
         if (task.isSuccessful) {
             continuation.resume(Unit)
         } else {
-            continuation.resumeWithException(task.exception ?: Exception("Failed to write $data to document $document"))
+            continuation.resumeWithException(task.exception ?: Exception("Failed to set $data to document $document"))
         }
     }
 }
 
-suspend fun <T : Any> DocumentReference.setValue(data: T) = setDocumentValue(this, data)
+/**
+ *
+ */
+suspend fun <T : Any> DocumentReference.set(data: T, setOptions: SetOptions? = null) = setValue(this, data)
+
+/**
+ *
+ */
+suspend fun DocumentReference.setMap(data: Map<String, Any>, setOptions: SetOptions? = null) = set(data, setOptions)
+//endregion
+
+//region UPDATE
+/**
+ *
+ */
+private suspend fun updateValue(document: DocumentReference, data: Map<String, Any>): Unit = suspendCoroutine { continuation ->
+    document.update(data).addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            continuation.resume(Unit)
+        } else {
+            continuation.resumeWithException(task.exception ?: Exception("Failed to update document $document with value $data"))
+        }
+    }
+}
+
+/**
+ *
+ */
+suspend fun DocumentReference.update(data: Map<String, Any>) = updateValue(this, data)
+
+/**
+ *
+ */
+suspend fun DocumentReference.update(vararg fields: Pair<String, Any>) = update(fields.toMap())
+//endregion
+
+//region DELETE
+/**
+ *
+ */
+private suspend fun deleteValue(document: DocumentReference): Unit = suspendCoroutine { continuation ->
+    document.delete().addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            continuation.resume(Unit)
+        } else {
+            continuation.resumeWithException(task.exception ?: Exception("Failed to delete document $document"))
+        }
+    }
+}
+
+/**
+ *
+ */
+suspend fun DocumentReference.delete() = deleteValue(this)
+//endregion
