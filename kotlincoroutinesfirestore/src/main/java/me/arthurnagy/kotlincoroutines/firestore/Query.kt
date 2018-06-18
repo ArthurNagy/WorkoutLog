9b833d8c1d6938bc0441b258b3b4408b0dd94eaa
Arthur <arthur.93.nagy@gmail.com>
2018-06-19 00:16:37 +0300

    Replace local module with JCenter dependency for the firestore kotlin extensions

--- kotlincoroutinesfirestore/src/main/java/me/arthurnagy/kotlincoroutines/firestore/Query.kt
+++
@@ -1,6 +0,0 @@
-package me.arthurnagy.kotlincoroutines.firestore
-
-import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.Source
-
-suspend inline fun <reified T> Query.awaitList(source: Source = Source.DEFAULT): List<T> = this.get(source).awaitList()
