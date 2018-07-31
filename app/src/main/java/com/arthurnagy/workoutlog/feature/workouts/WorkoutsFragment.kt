a4818ba623c48508c86c87a932516b368bd8c14f
Arthur Nagy <arthur.93.nagy@gmail.com>
2018-07-31 22:04:30 +0300

    Dynamically set the elevation of the appbar on the My Workouts screen

--- app/src/main/java/com/arthurnagy/workoutlog/feature/workouts/WorkoutsFragment.kt
+++ app/src/main/java/com/arthurnagy/workoutlog/feature/workouts/WorkoutsFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
+import com.arthurnagy.workoutlog.R
 import com.arthurnagy.workoutlog.WorkoutsBinding
 import com.arthurnagy.workoutlog.feature.shared.WorkoutLogFragment
 import com.arthurnagy.workoutlog.feature.shared.provideViewModel
+import com.google.android.material.appbar.AppBarLayout
 import javax.inject.Inject
 
 class WorkoutsFragment : WorkoutLogFragment() {
@@ -23,7 +26,14 @@ class WorkoutsFragment : WorkoutLogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = provideViewModel<WorkoutsViewModel>(viewModelFactory)
-
+        val appBarElevation = resources.getDimensionPixelSize(R.dimen.app_bar_elevation)
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, offset ->
+            val currentElevation: Float = if (offset == 0) 0f else appBarElevation.toFloat()
+            if (ViewCompat.getElevation(appBarLayout) != currentElevation) ViewCompat.setElevation(
+                appBarLayout,
+                currentElevation
+            )
+        })
     }
 
 }
