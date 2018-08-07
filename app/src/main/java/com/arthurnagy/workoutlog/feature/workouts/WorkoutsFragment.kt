65a7dbad43e88c34fe52eb8022153d7edba53c2b
Arthur Nagy <arthur.93.nagy@gmail.com>
2018-08-07 17:37:07 +0300

    Implement UI for selected week view

--- app/src/main/java/com/arthurnagy/workoutlog/feature/workouts/WorkoutsFragment.kt
+++ app/src/main/java/com/arthurnagy/workoutlog/feature/workouts/WorkoutsFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
+import com.arthurnagy.workoutlog.R
 import com.arthurnagy.workoutlog.WorkoutsBinding
 import com.arthurnagy.workoutlog.feature.shared.WorkoutLogFragment
 import com.arthurnagy.workoutlog.feature.shared.provideViewModel
+import com.google.android.material.appbar.AppBarLayout
 import javax.inject.Inject
+import kotlin.math.absoluteValue
 
 class WorkoutsFragment : WorkoutLogFragment() {
 
@@ -23,7 +27,13 @@ class WorkoutsFragment : WorkoutLogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = provideViewModel<WorkoutsViewModel>(viewModelFactory)
-
+        val appBarElevation = resources.getDimensionPixelSize(R.dimen.app_bar_elevation).toFloat()
+        val toolbarHeight = resources.getDimensionPixelSize(R.dimen.toolbar_height)
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, offset ->
+            val appBarOffset = offset.absoluteValue
+            val currentElevation = if (appBarOffset >= toolbarHeight) appBarElevation else 0f
+            if (ViewCompat.getElevation(appBarLayout) != currentElevation) ViewCompat.setElevation(appBarLayout, currentElevation)
+        })
     }
 
 }
